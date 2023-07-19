6d9157675898b71d4db3c6c86b876452e0928de7
Jack Gumoes <j.gumoes@gmail.com>
2023-07-19 15:10:06 +0100

    finished testing for BCD conversion methods

--- tests/BCD_conversion_tests.cpp
+++ tests/BCD_conversion_tests.cpp
@@ -9,14 +9,46 @@
 MockWire emptyWire = MockWire();
 RTCInterfaceClass<MockWire> RTC = RTCInterfaceClass(emptyWire);
 
+// decToBcd tests
+
 TEST(decToBcd, converts_9){
   EXPECT_EQ(RTC.decToBcd(9), 9);
 }
 
 TEST(decToBcd, converts_10){
-  EXPECT_EQ(RTC.decToBcd(10), 0b00010000);
+  EXPECT_EQ(RTC.decToBcd(10), 1 << 4);
+}
+
+TEST(decToBcd, converts_49){
+  EXPECT_EQ(RTC.decToBcd(49), 9 | (4 << 4));
+}
+
+TEST(decToBcd, converts_50){
+  EXPECT_EQ(RTC.decToBcd(50), (5 << 4));
 }
 
 TEST(decToBcd, converts_59){
-  EXPECT_EQ(RTC.decToBcd(59), 0b00010000);
+  EXPECT_EQ(RTC.decToBcd(59), 9 | (5 << 4));
+}
+
+// bcd2bin tests
+
+TEST(bcd2bin, converts_9){
+  EXPECT_EQ(RTC.bcd2bin(9), 9);
+}
+
+TEST(bcd2bin, converts_10){
+  EXPECT_EQ(RTC.bcd2bin(1 << 4), 10);
+}
+
+TEST(bcd2bin, converts_49){
+  EXPECT_EQ(RTC.bcd2bin(9 | (4 << 4)), 49);
+}
+
+TEST(bcd2bin, converts_50){
+  EXPECT_EQ(RTC.bcd2bin((5 << 4)), 50);
+}
+
+TEST(bcd2bin, converts_59){
+  EXPECT_EQ(RTC.bcd2bin(9 | (5 << 4)), 59);
 }
